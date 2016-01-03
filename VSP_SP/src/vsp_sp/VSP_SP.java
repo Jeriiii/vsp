ab11000f1387bef02eb55853b7d596d8aa8b1cb6
Petr Kukrál <p.kukral@jkbusiness.cz>
2016-01-03 14:42:05 +0100

    založení projektu

---
+++ VSP_SP/src/vsp_sp/VSP_SP.java
@@ -0,0 +1,19 @@
+/*
+ * VSP - Semestrální práce.
+ */
+package vsp_sp;
+
+/**
+ *
+ * @author Petr Kukrál <p.kukral@kukral.eu>
+ */
+public class VSP_SP {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+	}
+
+}
